154161a019f2bb4cf92cea79a5645c5b13e99757
LeonChibanda <leonchibanda@icloud.com>
2025-03-10 20:24:15 +0200

    A place I live in (South Africa, Cape Town)

---
+++ scripts/place.js
@@ -0,0 +1,40 @@
+// Static values for temperature and wind speed
+document.addEventListener("DOMContentLoaded", function () {
+    const temperature = 31; // in °C
+    const windSpeed = 7; // in km/h
+
+    // Func - calculate wind chill factor
+    function calculateWindChill(temp, wind) {
+        return 13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16);
+    }
+
+    function isWindChillApplicable(temp, wind) {
+        return temp <= 10 && wind > 4.8;
+    }
+
+    // Display - wind chill factor
+    const windChillElement = document.createElement("tr");
+    const windChillLabel = document.createElement("td");
+    windChillLabel.className = "label";
+    windChillLabel.innerHTML = "<b>Wind Chill:</b>";
+
+    const windChillValue = document.createElement("td");
+    if (isWindChillApplicable(temperature, windSpeed)) {
+        const windChill = calculateWindChill(temperature, windSpeed).toFixed(2);
+        windChillValue.textContent = `${windChill} °C`;
+    } else {
+        windChillValue.textContent = "N/A";
+    }
+
+    windChillElement.appendChild(windChillLabel);
+    windChillElement.appendChild(windChillValue);
+
+    document.querySelector(".weather table").appendChild(windChillElement);
+});
+
+// Footer infor
+
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
